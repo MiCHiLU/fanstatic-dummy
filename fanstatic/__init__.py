fccae1d8e65a681b229caae89b56bc9a575bfc58
MiCHiLU <djmchl@gmail.com>
2012-08-14 13:09:52 +0900

    Remove

--- fanstatic/__init__.py
+++ fanstatic/__init__.py
@@ -1,37 +1,17 @@
-from fanstatic.core import (Library,
-                            Resource,
-                            Slot,
-                            Group,
-                            GroupResource,
-                            NeededResources,
-                            DEFAULT_SIGNATURE,
-                            VERSION_PREFIX,
-                            BUNDLE_PREFIX,
-                            DEBUG,
-                            MINIFIED,
-                            NEEDED,
-                            sort_resources,
-                            bundle_resources,
-                            init_needed,
-                            del_needed,
-                            get_needed,
-                            clear_needed,
-                            register_inclusion_renderer,
-                            UnknownResourceExtensionError,
-                            UnknownResourceExtension, # BBB
-                            LibraryDependencyCycleError,
-                            ConfigurationError,
-                            SlotError,
-                            set_resource_file_existence_checking,
-                            UnknownResourceError)
+__all__ = ["Library", "Resources", "Groups", "GroupResource"]
 
-from fanstatic.registry import get_library_registry, LibraryRegistry
+class Dummy(object):
+  def __init__(self, *argv, **keargv):
+    return super(Dummy, self).__init__()
 
-from fanstatic.codegen import generate_code
+class Library(Dummy):
+  pass
 
-from fanstatic.injector import Injector, make_injector
+class Resources(Dummy):
+  pass
 
-from fanstatic.publisher import (Publisher, Delegator, make_publisher,
-                                 LibraryPublisher)
+class Group(Dummy):
+  pass
 
-from fanstatic.wsgi import Fanstatic, make_fanstatic, Serf, make_serf
+class GroupResource(Dummy):
+  pass
--- setup.py
+++ setup.py
@@ -1,35 +1,11 @@
 from setuptools import setup
 
-long_description = (
-    open('README.txt').read()
-    + '\n' +
-    open('CHANGES.txt').read())
-
 setup(
     name='fanstatic',
     version='0.13dev',
-    description="Flexible static resources for web applications.",
-    classifiers=[],
-    keywords='',
-    author='Fanstatic Developers',
-    author_email='fanstatic@googlegroups.com',
-    long_description=long_description,
+    description="Flexible static resources for web applications. (Dummy)",
+    long_description=open('README.txt').read(),
     license='BSD',
-    url='http://fanstatic.org',
+    url='https://github.com/MiCHiLU/fanstatic-dummy',
     packages=['fanstatic'],
-    include_package_data=True,
-    zip_safe=False,
-    install_requires=['Paste', 'WebOb'],
-    extras_require = dict(
-        test=['pytest >= 2.0'],
-        ),
-    entry_points = {
-        'paste.filter_app_factory': [
-            'fanstatic = fanstatic:make_fanstatic',
-            'publisher = fanstatic:make_publisher',
-            'injector = fanstatic:make_injector',
-            ],
-        'paste.app_factory': [
-            'serf = fanstatic:make_serf',
-            ],
-    })
+    )
